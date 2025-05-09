4e876bcdb164fd8a74e88e6269d7d949589b087d
henddiab <henddiab132@gmail.com>
2025-05-09 23:00:52 +0300

    add rating component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,21 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
   ],
 };
--- src/app/shared/components/rating/rating.component.ts
+++ src/app/shared/components/rating/rating.component.ts
@@ -1,18 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rating',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './rating.component.html',
-  styleUrl: './rating.component.scss'
+  styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent {
-  @Input() rating: any;
+  @Input() rate: number = 0;
+  readonly maxStars = 5;
 
-
-  get stars() {
-    return Array(Math.floor(this.rating)).fill(0);
+  get fullStars(): number[] {
+    return Array(Math.floor(this.rate)).fill(0);
   }
 
+  get hasHalfStar(): boolean {
+    return this.rate % 1 >= 0.25 && this.rate % 1 < 0.75;
+  }
+
+  get emptyStars(): number[] {
+    const full = Math.floor(this.rate);
+    const half = this.hasHalfStar ? 1 : 0;
+    return Array(this.maxStars - full - half).fill(0);
+  }
 }
