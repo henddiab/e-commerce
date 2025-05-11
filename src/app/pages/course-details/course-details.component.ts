496ec947a1693ffb7b01253760e4cb456d029baf
henddiab <henddiab132@gmail.com>
2025-05-11 20:06:58 +0300

    enhance performance

--- src/app/pages/course-details/course-details.component.ts
+++ src/app/pages/course-details/course-details.component.ts
@@ -2,7 +2,6 @@ import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CoursesService } from '../../core/services/courses/courses.service';
 import { CourseData } from '../../shared/models/courseData.interface';
-import { HttpClient } from '@angular/common/http';
 import { RatingComponent } from '../../shared/components/rating/rating.component';
 import { BreadcrumbComponent } from '../../shared/components/breadcrumb/breadcrumb.component';
 import { BreadcrumbItem } from '../../shared/models/breadcrumb.interface';
@@ -10,7 +9,6 @@ import { BreadcrumbItem } from '../../shared/models/breadcrumb.interface';
 @Component({
   selector: 'app-course-details',
   imports: [RatingComponent, BreadcrumbComponent],
-  providers: [HttpClient],
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.scss',
 })
